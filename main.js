a38adcff6816a924981e0bce50b3dd25cab79d85
jackvance008 <jackvance008@gmail.com>
2024-12-04 14:13:17 -0800

    Update main.js

--- main.js
+++ main.js
@@ -10,13 +10,24 @@ let s_key_down = false;
 let d_key_down = false;
 let space_key_down = false;
 
+const block = new Block(canvas.width / 2, canvas.height / 2);
+
 function loop()
 {
-  //Background clear
+	//Background clear
 	c.beginPath();
 	c.fillStyle = "gray";
 	c.rect(0, 0, canvas.width, canvas.height);
 	c.fill();
+
+	block.draw();
+	block.update();
+
+	//Movement
+	if(w_key_down){block.moveUp();}
+	if(a_key_down){block.moveLeft();}
+	if(s_key_down){block.moveDown();}
+	if(d_key_down){block.moveRight();}
 }
 
 loop();
